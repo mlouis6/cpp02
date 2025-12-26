a7a038da80478cdba9a0e2d11b27f6dedd2dc9ca
ML <louism0495@gmail.com>
2025-12-26 18:20:00 +0100

    ex00 skeleton

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 18:14:03 by mlouis            #+#    #+#             */
+/*   Updated: 2025/12/26 18:19:37 by mlouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+class Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(const Fixed&);
+		Fixed& operator=(const Fixed&);
+		~Fixed(void);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+	private:
+		int					_nb;
+		static const int	_frac;
+};
+
+#endif
